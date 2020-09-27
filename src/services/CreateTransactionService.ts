3365fc8c79c572c964ef8f41f4fda38b0144322b
Marcelo de Borba <marcelo.deborba@hotmail.com>
2020-09-27 17:55:58 -0300

    Resolução desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+
+    // Validando o tipo de transação
+    if (!['income', 'outcome'].includes(type)){
+      throw new Error('Este tipo de transação não é válida!');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === "outcome" && total < value){
+      throw new Error('Eu não tenho saldo suficiente para este saque');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transaction;
   }
 }
 
